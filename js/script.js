f7cb822be7dd00b326d4fb9d544a4be17158b1bb
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2024-11-15 21:48:39 +0100

    Primer Pokemon Llamado por Completo

--- js/script.js
+++ js/script.js
@@ -5,16 +5,32 @@ const urlPokemon = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`;
 
 getPokemon.addEventListener("click", () => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
+    
     .then((response) => {
         if(!response.ok)
             throw new Error("No es posible cargar el Pokémon")
                 return response.json()
     })
+    
     .then ((data) => {
         console.log(data)
-        bulbasaur.innerHTML = `<h2>${data.forms[0].name}</h2>`
+        let crearDiv = document.querySelector("pokemon-info") // creo una variable de algo que no existe
+        if (!crearDiv) {//si no existe...
+            crearDiv = document.createElement("div") // lo creo
+            crearDiv.className = "pokemon-info" // le doy clase
+            document.querySelector(".pokemon-form").appendChild(crearDiv) // lo hago hijo del form en una sola linea
+        }
+        crearDiv.innerHTML = `
+        <h2>${data.name.toUpperCase()}</h2>
+        <img src=${data.sprites.front_default} alt="${data.name}">
+        <h3>Tipo: ${data.types[0].type.name}</h3>
+        <h3>Altura: ${data.height} Ft</h3>
+        <h3>Peso: ${data.weight} Kg.</h3>
+        `
+
         })
-    .catch((error) => {
+    
+        .catch((error) => {
         console.error(error);
-    })
+        })
     })
